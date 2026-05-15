ecdc47e36c28518548bcf2d0b1b15dc45384a7df
Feras Ahmad <firastaf878@gmail.com>
2026-05-15 15:43:58 -0400

    minor

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout() {
       <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
         <Stack>
           <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-          <Stack.Screen name="park/[id]" options={{ title: 'Park Details' }} />
+          <Stack.Screen name="park/[id]" options={{ title: 'Park Details'}} />
         </Stack>
         <StatusBar style="auto" />
       </ThemeProvider>
